562681acc7999e3ca2f045dac3cdc99e3099d845
YammD123 <siamqueen52@gmail.com>
2025-10-30 09:39:37 +0700

    feat: menambahkan migration student

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [LandingController::class, 'index'])->name('landing');
 
 Route::prefix('admin')->group(function () {
-
     Route::get('/dashboard', [AdminController::class, 'index'])->name('admin');
 });
